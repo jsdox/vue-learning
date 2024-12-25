ccbc86285a0f4e5e440c510ca14b1c181964d2f5
jsdox <jatindersingh@founderandlightning.com>
2024-12-25 16:42:03 +0530

    setting props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,10 +1,25 @@
 export default
 {
     template: `
-        <button :disabled="processing">
+        <button 
+            :class="{ 
+                'btn': true,
+                'btn-primary': type === 'primary',
+                'btn-secondary': type === 'secondary',
+                'btn-muted': type === 'muted', 
+            }"
+            :disabled="processing"
+        >
             <slot/>
         </button>
     `,
+
+    props: {
+        'type': {
+            type: String,
+            default: 'primary'
+        }
+    },
     data() {
         return {
             processing: true
