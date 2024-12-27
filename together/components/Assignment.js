a777b44b0b5e668611bc87161ac7e5776c9a4a80
jsdox <jatindersingh@founderandlightning.com>
2024-12-27 11:05:14 +0530

    component separation

---
+++ together/components/Assignment.js
@@ -0,0 +1,20 @@
+export default {
+    template: `
+<!--        <li v-for="assignment in assignments" :key="assignment.id">-->
+<!--            <label>-->
+<!--                {{ assignment.name }}-->
+<!--                <input type="checkbox" v-model="assignment.completed">-->
+<!--            </label>-->
+<!--        </li>-->
+            <li :key="assignment.id">
+                <label>
+                    {{ assignment.name }}
+                    <input type="checkbox" v-model="assignment.completed">
+                </label>
+            </li>
+    `,
+
+    props:{
+        assignment: Array,
+    }
+}
